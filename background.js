b6afc49962458b3e1c58bd3d23d518e7edd1abf3
Tomáš Taro <tomas@getadblock.com>
2015-11-26 21:49:42 +0100

    Don't redefine *storage*
    Redefining of a let leads to an exception.

--- background.js
+++ background.js
@@ -3,17 +3,17 @@
 // Add sites to storage
 function addSites(tabs, callback) {
     chrome.storage.local.get("sites", function(storage) {
-        let storage = storage["sites"] || [];
+        let sites = storage["sites"] || [];
         for (let tab of tabs) {
             // Get a favicon properly
             if (!tab.favIconUrl || tab.favIconUrl.indexOf("chrome://theme") > -1) {
                 tab.favIconUrl = chrome.runtime.getURL("../img/favicon.png");
             }
             let site = { title: tab.title, faviconUrl: tab.favIconUrl, url: tab.url };
-            storage.push(site);
+            sites.push(site);
         }
         // Save modified |storage| object
-        chrome.storage.local.set({sites: storage}, function() {
+        chrome.storage.local.set({sites}, function() {
             callback();
         });
     });
